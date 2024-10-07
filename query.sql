084fd6a388639fb8853ec0ed92bb57981396c85c
brandonmateos <blozano0@ucol.mx>
2024-10-07 16:03:33 -0600

    covert img to webp

--- query.sql
+++ query.sql
@@ -12,16 +12,16 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (name, brand, code, category, availability, price, image) VALUES
-('Desbrozadora a gasolina 26cc', 'Truper', 'DES-26C', 'Jardineria', TRUE, 9999.99, 'img/products/DES-26C.jpg'),
-('Rotomartillo inal. 1/2", 20V', 'Truper', 'ROTI-20A', 'Máquinas portátiles inalámbricas', TRUE, 9999.99, 'img/ROTI-20A.jpg'),
-('Rotomartillo SDS Plus 5 Joules', 'Truper Industrial', 'ROEL-50N2', 'Máquinas portátiles', TRUE, 9999.99, 'img/products/ROEL-50N2.jpg'),
-('Hacha cazadora 1 lb mango de nylon de 14"', 'Truper', 'HVE-1', 'Hachas', TRUE, 9999.99, 'img/products/HVE-1.jpg'),
-('Cargador c/arrancador de 50 A', 'Truper', 'CARBA-50', 'Accesorios para auto', TRUE, 9999.99, 'img/products/CARBA-50.jpg'),
-('Compresor de aire, lubricado 20L 2½HP', 'Pretul', 'COMP-20LP', 'Compresores y accesorios', TRUE, 9999.99, 'img/products/COMP-20LP.jpg'),
-('Esmeriladora angular 4-1/2", 750W', 'Truper', 'ERGO-4570', 'Máquinas portátiles', TRUE, 9999.99, 'img/products/ERGO-4570.jpg'),
-('Go Find 22', 'Minelab', 'GOFINE-22', 'Detector de Metales', TRUE, 9999.99, 'img/products/goFind22.jpg'),
+('Desbrozadora a gasolina 26cc', 'Truper', 'DES-26C', 'Jardineria', TRUE, 9999.99, 'img/products/DES-26C.webp'),
+('Rotomartillo inal. 1/2", 20V', 'Truper', 'ROTI-20A', 'Máquinas portátiles inalámbricas', TRUE, 9999.99, 'img/ROTI-20A.webp'),
+('Rotomartillo SDS Plus 5 Joules', 'Truper Industrial', 'ROEL-50N2', 'Máquinas portátiles', TRUE, 9999.99, 'img/products/ROEL-50N2.webp'),
+('Hacha cazadora 1 lb mango de nylon de 14"', 'Truper', 'HVE-1', 'Hachas', TRUE, 9999.99, 'img/products/HVE-1.webp'),
+('Cargador c/arrancador de 50 A', 'Truper', 'CARBA-50', 'Accesorios para auto', TRUE, 9999.99, 'img/products/CARBA-50.webp'),
+('Compresor de aire, lubricado 20L 2½HP', 'Pretul', 'COMP-20LP', 'Compresores y accesorios', TRUE, 9999.99, 'img/products/COMP-20LP.webp'),
+('Esmeriladora angular 4-1/2", 750W', 'Truper', 'ERGO-4570', 'Máquinas portátiles', TRUE, 9999.99, 'img/products/ERGO-4570.webp'),
+('Go Find 22', 'Minelab', 'GOFINE-22', 'Detector de Metales', TRUE, 9999.99, 'img/products/goFind22.webp'),
 ('Simplex+', 'Nokta Makro', 'SIMPLEX+', 'Detector de Metales', FALSE, 9999.99, 'img/products/simplex+.webp'),
-('Tecknetics Eurotek Pro', 'Teknetics', 'EURO-PRO', 'Detector de Metales', TRUE, 9999.99, 'img/products/tekneticsEurotekPro.jpg'),
+('Tecknetics Eurotek Pro', 'Teknetics', 'EURO-PRO', 'Detector de Metales', TRUE, 9999.99, 'img/products/tekneticsEurotekPro.webp'),
 ('Rifle Beeman / Marksman', 'Beeman', '2063', 'Rifle Aire', TRUE, 9999.99, 'img/products/rifleBeeman.webp'),
 ('Rifle Pcp V-raptor Polimero Regulado Cal 5.5', 'Aztk', 'Pr900S', 'Rifle Pcp', TRUE, 9999.99, 'img/products/riflePcpAztk.webp'),
-('Pistola Deportiva Cañon Corto Cal 4.5 ', 'Mendoza', 'PK-62-C-0227', 'Pistola de Salva', TRUE, 9999.99, 'PK-62-C-0227.png');
+('Pistola Deportiva Cañon Corto Cal 4.5 ', 'Mendoza', 'PK-62-C-0227', 'Pistola de Salva', TRUE, 9999.99, 'PK-62-C-0227.webp');
